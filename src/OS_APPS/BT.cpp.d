c49286f30cad24ab73fb1b79601503bd43ef04ca
TheTechnician <supersixtyone@gmail.com>
2026-02-16 12:31:23 -0500

    File Upload

---
+++ src/OS_APPS/BT.cpp.d
@@ -0,0 +1,1 @@
+.pio/build/OTA_APP/src/OS_APPS/BT.cpp.o: src/OS_APPS/BT.cpp
---
+++ src/assets.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/OTA_APP/src/assets.cpp.o: src/assets.cpp include/assets.h \
+ C:/Users/super/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
